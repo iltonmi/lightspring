2ef589c9c0db03a81359a90dfebc9a03d1aefa76
shutUpAndThinkHard <linweili666@163.com>
2019-10-02 12:37:50 +0800

    Adding BeanDefinitionValueResolver, which can return a value based on the type of property value, such as RuntimeBeanReference and TypedStringValue.

---
+++ src/main/java/org/lightspring/beans/factory/config/RuntimeBeanReference.java
@@ -0,0 +1,11 @@
+package org.lightspring.beans.factory.config;
+
+public class RuntimeBeanReference {
+	private final String beanName;
+	public RuntimeBeanReference(String beanName) {
+		this.beanName = beanName;
+	}
+	public String getBeanName() {
+		return this.beanName;
+	}
+}
