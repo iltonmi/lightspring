f274199da2f805303e07ec8e8e19166eb9f89467
shutUpAndThinkHard <linweili666@163.com>
2019-10-10 11:07:58 +0800

    Finish all test cases from v1 to v4.

--- src/main/java/org/lightspring/core/annotation/AnnotationUtils.java
+++ src/main/java/org/lightspring/core/annotation/AnnotationUtils.java
@@ -7,9 +7,13 @@ import java.lang.reflect.AnnotatedElement;
 public abstract class AnnotationUtils {
 
     public static <T extends Annotation> T getAnnotation(AnnotatedElement ae, Class<T> annotationType) {
+        //获取注解
         T ann = ae.getAnnotation(annotationType);
+        //若不存在此注解
         if (ann == null) {
+            //遍历注解
             for (Annotation metaAnn : ae.getAnnotations()) {
+                //获得注解的注解
                 ann = metaAnn.annotationType().getAnnotation(annotationType);
                 if (ann != null) {
                     break;
--- src/test/java/org/lightspring/test/AllTests.java
+++ src/test/java/org/lightspring/test/AllTests.java
@@ -6,9 +6,10 @@ import org.junit.runners.Suite.SuiteClasses;
 import org.lightspring.test.v1.V1AllTests;
 import org.lightspring.test.v2.V2AllTests;
 import org.lightspring.test.v3.V3AllTests;
+import org.lightspring.test.v4.V4AllTests;
 
 @RunWith(Suite.class)
-@SuiteClasses({V1AllTests.class,V2AllTests.class, V3AllTests.class})
+@SuiteClasses({V1AllTests.class, V2AllTests.class, V3AllTests.class, V4AllTests.class})
 public class AllTests {
 
 }
