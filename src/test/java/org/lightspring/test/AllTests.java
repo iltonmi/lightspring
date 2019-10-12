5dd5413ca1492166e01443582282fec9113f27e5
shutUpAndThinkHard <linweili666@163.com>
2019-10-12 22:25:58 +0800

    Using Java dynamic proxy implemented aop.

--- src/test/java/org/lightspring/test/AllTests.java
+++ src/test/java/org/lightspring/test/AllTests.java
@@ -8,9 +8,12 @@ import org.lightspring.test.v2.V2AllTests;
 import org.lightspring.test.v3.V3AllTests;
 import org.lightspring.test.v4.V4AllTests;
 import org.lightspring.test.v5.V5AllTests;
+import org.lightspring.test.v6.V6AllTests;
 
 @RunWith(Suite.class)
-@SuiteClasses({V1AllTests.class, V2AllTests.class, V3AllTests.class, V4AllTests.class, V5AllTests.class})
+@SuiteClasses({V1AllTests.class, V2AllTests.class,
+        V3AllTests.class, V4AllTests.class,
+        V5AllTests.class, V6AllTests.class})
 public class AllTests {
 
 }
